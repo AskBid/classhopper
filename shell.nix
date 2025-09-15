cb1ff0ef0ad09b8f58963a609da0490ada9812e1
AskBid <noreply>
2025-09-15 17:46:55 +0100

    multi folder project with good setup for matchin ghc and hls from nix config

--- shell.nix
+++ shell.nix
@@ -4,7 +4,11 @@ pkgs.mkShell {
   buildInputs = [
     pkgs.haskell.compiler.ghc96
     pkgs.cabal-install
-    pkgs.haskellPackages.haskell-language-server
+    pkgs.haskell.packages.ghc96.haskell-language-server
     pkgs.haskellPackages.hlint
+    pkgs.SDL2
+    pkgs.pkg-config
+    pkgs.libGL
+    pkgs.libGLU
   ];
 }
