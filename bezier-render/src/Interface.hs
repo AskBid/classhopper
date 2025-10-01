c929620adcc17f579b1e9e171fc64ea9be11889d
AskBid <noreply>
2025-10-01 20:55:17 +0100

    add arrow keys rotation.

--- bezier-render/src/Interface.hs
+++ bezier-render/src/Interface.hs
@@ -3,9 +3,10 @@
 module Interface where
 
 import Graphics.UI.GLFW
-import Control.Monad (unless)
+import Control.Monad (unless, when)
 import qualified Graphics.Rendering.OpenGL as GL
 import Prelude hiding (init)
+import Data.IORef
 
 import qualified Drawing as D
 
@@ -22,13 +23,17 @@ launchWindow = do
         Just win -> do
           makeContextCurrent (Just win)
           swapInterval 1
-          -- _ <- initVAO
-          appLoop win
+
+          -- rotation state
+          rotZ <- newIORef 0.0
+          setKeyCallback win (Just (keyHandler rotZ))
+          
+          appLoop win rotZ
           destroyWindow win
           terminate
 
-appLoop :: Window -> IO ()
-appLoop window = do
+appLoop :: Window -> IORef GL.GLfloat -> IO ()
+appLoop window rotZRef = do
   shouldClose <- windowShouldClose window
   unless shouldClose $ do
     pollEvents
@@ -38,11 +43,12 @@ appLoop window = do
     setupOrtho   
 
     GL.viewport GL.$= (GL.Position 0 0, GL.Size 800 600)
-    setAssonometricView
+    rotZ <- readIORef rotZRef
+    setAssonometricView rotZ
     D.drawing
 
     swapBuffers window
-    appLoop window
+    appLoop window rotZRef
 
 setupOrtho :: IO ()
 setupOrtho = do
@@ -53,9 +59,18 @@ setupOrtho = do
   GL.matrixMode GL.$= GL.Modelview 0
   GL.loadIdentity
 
-setAssonometricView :: IO ()
-setAssonometricView = do
+setAssonometricView :: GL.GLfloat -> IO ()
+setAssonometricView rotZ = do
   GL.loadIdentity
-  GL.rotate (45) (GL.Vector3 1 0 0 :: GL.Vector3 GL.GLfloat)
-  GL.rotate (45) (GL.Vector3 0 0 1 :: GL.Vector3 GL.GLfloat)
-  -- GL.rotate 80 (GL.Vector3 1 0 0 :: GL.Vector3 GL.GLfloat)         -- rotate sideways
+  GL.rotate 45 (GL.Vector3 1 0 0 :: GL.Vector3 GL.GLfloat)
+  GL.rotate 45 (GL.Vector3 0 0 1 :: GL.Vector3 GL.GLfloat)
+  GL.rotate rotZ (GL.Vector3 0 0 1 :: GL.Vector3 GL.GLfloat)
+
+-- Key handler: modify rotation angle
+keyHandler :: IORef GL.GLfloat -> Window -> Key -> Int -> KeyState -> ModifierKeys -> IO ()
+keyHandler rotZRef _win key scancode action _mods = 
+  when (action == KeyState'Pressed || action == KeyState'Repeating) $
+    case key of
+      Key'Right -> modifyIORef' rotZRef (+5)
+      Key'Left  -> modifyIORef' rotZRef (subtract 5)
+      _         -> return ()
